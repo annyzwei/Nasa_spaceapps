dd77a26dd96217da818bc68ebe983690228aaa7c
linazhuge <linazhuge18@gmail.com>
2025-10-04 17:28:36 -0400

    frontend changes

--- spike/frontend/src/app/layout.tsx
+++ spike/frontend/src/app/layout.tsx
@@ -1,9 +1,11 @@
 import ThemeRegistry from "./ThemeRegistry";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.variable}>
         <ThemeRegistry>{children}</ThemeRegistry>
       </body>
     </html>
--- spike/frontend/src/app/page.tsx
+++ spike/frontend/src/app/page.tsx
@@ -6,12 +6,21 @@ import {Button, Input, TextField, Typography} from "@mui/material";
 import { useEffect, useState } from "react";
 
 import ThreeScene from "../components/componentJs.jsx"
+import MainView from "../components/mainView";
+import TimeScaledTimeline from "../components/SimpleTimeline";
 
 export default function Home() {
 
   const [msg, setMsg] = useState("");
   const [search, setSearchVal] = useState("");
   const [color, setColor] = useState("red");
+
+  const items = [
+      { when: "2025-01-10", title: "Plan" },
+      { when: "2025-02-02", title: "Design" },
+      { when: "2025-03-12", title: "Build" },
+      { when: "2025-04-20", title: "Launch" },
+    ];
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/api/hello")
@@ -21,7 +30,7 @@ export default function Home() {
 
   return (
     <>
-    <div className={styles.page} style={{backgroundColor: color}}>
+    {/* <div className={styles.page} style={{backgroundColor: color}}>
       <Typography variant="h1">SPIKE!</Typography>
       <Typography variant="h5">{msg + search}</Typography>
       <TextField
@@ -31,7 +40,9 @@ export default function Home() {
         fullWidth
       />
       <Button onClick={() => {if (color == "red") setColor("green"); else setColor("red")}} sx={{backgroundColor: "white"}}></Button>
-    </div>
+    </div> */}
+    <MainView/>
+    <TimeScaledTimeline items={items} />
     <ThreeScene/>
   </>
   );
