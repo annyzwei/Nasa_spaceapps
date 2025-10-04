d2e1271ed869e47be126b9a81554047ce031dc80
Anny Wei <annywei9@gmail.com>
2025-10-04 13:15:36 -0400

    upgraded website

--- spike/src/app/page.tsx
+++ spike/src/app/page.tsx
@@ -2,14 +2,16 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import {Typography} from "@mui/material";
+import {Button, Input, TextField, Typography} from "@mui/material";
 import { useEffect, useState } from "react";
 
-
+import ThreeScene from "./../components/componentJs.jsx"
 
 export default function Home() {
 
   const [msg, setMsg] = useState("");
+  const [search, setSearchVal] = useState("");
+  const [color, setColor] = useState("red");
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/api/hello")
@@ -18,9 +20,19 @@ export default function Home() {
   }, []);
 
   return (
-    <div className={styles.page}>
+    <>
+    <div className={styles.page} style={{backgroundColor: color}}>
       <Typography variant="h1">SPIKE!</Typography>
-      <Typography variant="h5">{msg}</Typography>
+      <Typography variant="h5">{msg + search}</Typography>
+      <TextField
+        label="search"
+        value={search}
+        onChange={(e) => setSearchVal(e.target.value)}
+        fullWidth
+      />
+      <Button onClick={() => {if (color == "red") setColor("green"); else setColor("red")}} sx={{backgroundColor: "white"}}></Button>
     </div>
+    <ThreeScene/>
+  </>
   );
 }
